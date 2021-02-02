1aba5bf12189da8254b44e6a84f56283c0692a2a
ewryden-official <ewryden@mtu.edu>
2021-02-02 00:41:25 -0500

    2.2

--- grails-app/init/cs4760progassign/BootStrap.groovy
+++ grails-app/init/cs4760progassign/BootStrap.groovy
@@ -13,6 +13,23 @@ class BootStrap {
                 .addToBooks(new Book(title:"Huckelberry Finn", publishYear:1884))
                 .save()
 
+        // Add for creating Roles and Users
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def testAdmin = new User(username: 'admin', password: 'password')
+        testAdmin.save(flush: true)
+
+        def testUser = new User(username: 'user', password: 'password')
+        testUser.save(flush: true)
+
+        UserRole.create testAdmin, adminRole, true
+        UserRole.create testUser, userRole, true
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
     }
     def destroy = {
     }
