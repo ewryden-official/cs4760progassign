50861e38c8431dd8c353f55ae765477096977afe
ewryden-official <ewryden@mtu.edu>
2021-02-02 22:39:14 -0500

    8

--- grails-app/assets/javascripts/showTime.js
+++ grails-app/assets/javascripts/showTime.js
@@ -1,7 +1,9 @@
+// Show time - Simple Ajax
 console.log("Hello showtime");
 $(document).ready( function(){
+    console.log("In document ready");
+    $('#time').load("/cs4760progassign/home/showTime");
     $('#timeLink').click(function(){
-        console.log("showtime click function");
         $('#time').load(this.href); return false;
     });
 });
