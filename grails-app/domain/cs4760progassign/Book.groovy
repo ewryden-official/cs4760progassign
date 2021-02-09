c26bbef1f92da77ec1318ddc313928a8f34629ae
ewryden-official <ewryden@mtu.edu>
2021-02-09 12:46:23 -0500

    1

--- grails-app/domain/cs4760progassign/Book.groovy
+++ grails-app/domain/cs4760progassign/Book.groovy
@@ -4,10 +4,15 @@ class Book {
 
     String title
     Integer publishYear
+    byte[] cover
 
     static belongsTo = [author: Author]
 
     static constraints = {
+        title nullable: false, blank: false
+        author nullable: false, blank: false
+        publishYear nullable: false
+        cover nullable: true, maxSize: 1024*1024*2
     }
 
     String toString(){
